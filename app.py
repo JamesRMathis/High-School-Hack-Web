0bd00764b56f1379ce232f3358ea187f13d1ae24
jamesrmathis <jrfmathis06@outlook.com>
2024-03-12 23:33:21 -0500

    working on gateway

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return send_from_directory('static', 'gateway.html')
     # return 'Hello, World!'
 
-@app.route('/login')
+@app.route('/', methods=['POST'])
+def index_post():
+    return redirect('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81')
+
+@app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81')
 def login():
-    return render_template('login.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
