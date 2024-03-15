5092ef8e1ec7669a4b9cc60c08c04e4b6969740f
Aproem1 <Arnavkarekar72034@gmail.com>
2024-03-14 21:41:27 -0500

    added directory traversal vulnerability

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, redirect
+from flask import Flask, render_template, send_from_directory, redirect, request, send_file
 
 app = Flask(__name__)
 
@@ -9,10 +9,23 @@ def index():
 
 @app.route('/', methods=['POST'])
 def index_post():
-    return redirect('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81')
+    return redirect('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81', filename="index.html")
+
+@app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a83')
+def login():
+    return render_template('login.html')
+
+@app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a82')
+def haha():
+    filename = request.args.get('filename')
+    filepath = f"./static/{filename}"  # This is insecure!
+    try:
+        return send_file(filepath)
+    except FileNotFoundError:
+        return "File not found", 404
 
 @app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81')
-def login():
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
