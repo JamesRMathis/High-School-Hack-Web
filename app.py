c3f7bc609859e947e7ab65930ab8fe34c706f193
jamesrmathis <jrfmathis06@outlook.com>
2024-03-22 00:18:31 -0500

    added login

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template, send_from_directory, redirect
+from flask import Flask, render_template, send_from_directory, redirect, request, jsonify
+import sqlite3
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT
+    )
+''')
 
 app = Flask(__name__)
 
@@ -12,8 +23,29 @@ def index_post():
     return redirect('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81')
 
 @app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81')
-def login():
+def mainPage():
     return render_template('index.html')
+
+@app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/bb170201ef5d8f4449fd06812f53dc3d970875ca2c25abbe2bfc3683db807a81/processLogin', methods=['POST'])
+def processLogin():
+    username = request.form['username']
+    password = request.form['password']
+
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(f'''
+        SELECT * FROM users WHERE username = '{username}' AND password = '{password}'
+    ''')
+    user = cursor.fetchone()
+
+    if user:
+        return jsonify({'status': 'success'})
+    else:
+        return jsonify({'status': 'failed'})
 
 if __name__ == '__main__':
     app.run(debug=True)
